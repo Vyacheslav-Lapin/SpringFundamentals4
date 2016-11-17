dfcc309dfdd412f9f53bb2c11e63476712956292
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-11-17 17:11:38 +0300

    lab 3 start

---
+++ helloWorld/src/main/java/lab/model/UsualPerson.java
@@ -0,0 +1,30 @@
+package lab.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class UsualPerson implements Person {
+    @Id
+    @Column
+    private int id;
+
+    @Column
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private int age;
+    private float height;
+    private boolean isProgrammer;
+    private List<String> contacts;
+
+    @Override
+    public void sayHello(Person person) {
+    }
+}
