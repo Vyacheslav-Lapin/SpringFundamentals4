fd2366d5db401e515b53814db176a780bea04463
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-11-23 16:47:53 +0300

    lab 5 start - 2

--- helloWorld/src/main/java/lab/model/Customer.java
+++ helloWorld/src/main/java/lab/model/Customer.java
@@ -1,31 +1,13 @@
 package lab.model;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
 public class Customer implements Person {
     private String name;
     private boolean broke;
-
-    public Customer() {
-    }
-
-    public Customer(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean isBroke() {
-        return broke;
-    }
-
-    public void setBroke(boolean broke) {
-        this.broke = broke;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public void sayHello(Person person) {
     }
